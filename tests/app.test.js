8efb0259c0a72d9d1c45164e506fa4a25c3acfa6
shurnster <slainuponmyshield@gmail.com>
2017-10-12 15:14:40 -0700

    Did re-wording and fixed a tyepo

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../components/App';
 
-describe('A a basic React Header', () => {
+describe('A basic React boilerplate header example', () => {
   const wrapper = shallow(<App />);
   it('should have an existing App component', () => {
     expect(wrapper.exists()).toBe(true);
